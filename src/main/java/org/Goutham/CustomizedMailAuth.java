d9afc6476ca78404d20d887923fb4d5462f3c99d
Goutham peddi <peddigoutham93172@gmail.com>
2023-06-11 16:36:52 +0530

    sendEmail

---
+++ src/main/java/org/Goutham/CustomizedMailAuth.java
@@ -0,0 +1,12 @@
+package org.Goutham;
+
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+
+public class CustomizedMailAuth extends Authenticator {
+
+    protected PasswordAuthentication getPasswordAuthentication(){
+        return new PasswordAuthentication(MailMetaData.myMail, MailMetaData.myPasswrd);
+    }
+
+}
---
+++ src/main/java/org/Goutham/Main.java
@@ -0,0 +1,13 @@
+package org.Goutham;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("preparing to send mail---Weekly Test");
+
+        MailHandler mailHandler=new MailHandler();
+        mailHandler.sndMail();
+
+    }
+}
